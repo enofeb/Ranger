0d57628cdfa96e5142a2706ff28c9c734c800d53
enofeb <eneszor95@gmail.com>
2021-09-29 14:41:59 +0300

    RangerView was updated with onMeasure

--- view/src/main/java/com/enofeb/view/RangerView.kt
+++ view/src/main/java/com/enofeb/view/RangerView.kt
@@ -10,4 +10,21 @@ class RangerView @JvmOverloads constructor(
     defStyleAttr: Int
 ) : View(context, attrs, defStyleAttr) {
 
+    private var barHeight: Int = 0
+    private var circleRadius: Int = 0
+
+    private fun measureWidth(measureSpec: Int): Int {
+        val size = paddingLeft + paddingRight
+        return resolveSizeAndState(size, measureSpec, 0)
+    }
+
+    private fun measureHeight(measureSpec: Int): Int {
+        var size = paddingTop + paddingBottom
+        size += barHeight.coerceAtLeast(circleRadius)
+        return resolveSizeAndState(size, measureSpec, 0)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        setMeasuredDimension(measureWidth(widthMeasureSpec), measureHeight(heightMeasureSpec))
+    }
 }
