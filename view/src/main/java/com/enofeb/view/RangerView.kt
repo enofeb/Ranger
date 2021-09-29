3290802b57199630327bf7b853b15837208999b8
enofeb <eneszor95@gmail.com>
2021-09-29 15:28:48 +0300

    Ranger view canvas was initialized

--- view/src/main/java/com/enofeb/view/RangerView.kt
+++ view/src/main/java/com/enofeb/view/RangerView.kt
@@ -1,6 +1,9 @@
 package com.enofeb.view
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 
@@ -10,8 +13,22 @@ class RangerView @JvmOverloads constructor(
     defStyleAttr: Int
 ) : View(context, attrs, defStyleAttr) {
 
-    private var barHeight: Int = 0
+    private var barHeight: Int = 15
     private var circleRadius: Int = 0
+    private var barBasePaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    init {
+        barBasePaint.color = context.getColor(R.color.colorPurple)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        setMeasuredDimension(measureWidth(widthMeasureSpec), measureHeight(heightMeasureSpec))
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        drawRangerBar(canvas)
+    }
 
     private fun measureWidth(measureSpec: Int): Int {
         val size = paddingLeft + paddingRight
@@ -24,7 +41,19 @@ class RangerView @JvmOverloads constructor(
         return resolveSizeAndState(size, measureSpec, 0)
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        setMeasuredDimension(measureWidth(widthMeasureSpec), measureHeight(heightMeasureSpec))
+    private fun drawRangerBar(canvas: Canvas) {
+        val paddingSpaces = paddingLeft - paddingRight
+        val barLength = (width - paddingSpaces).toFloat()
+        val barCenter = (height / 2).toFloat()
+
+        val halfBarHeight = (barHeight / 2).toFloat()
+        val top = barCenter - halfBarHeight
+        val bottom = barCenter + halfBarHeight
+        val left = paddingLeft.toFloat()
+        val right = paddingLeft + barLength
+        val rectangle = RectF(left, top, right, bottom)
+
+        canvas.drawRoundRect(rectangle, halfBarHeight, halfBarHeight, barBasePaint)
+
     }
 }
