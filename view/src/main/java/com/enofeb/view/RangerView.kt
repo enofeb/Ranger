437eb4dd4619ee5a5bb9c02a0e0badcded91e70f
enofeb <eneszor95@gmail.com>
2021-09-29 12:32:48 +0300

    Initial commit

---
+++ view/src/main/java/com/enofeb/view/RangerView.kt
@@ -0,0 +1,13 @@
+package com.enofeb.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class RangerView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int
+) : View(context, attrs, defStyleAttr) {
+
+}
